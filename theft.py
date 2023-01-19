e3eaa54be122e45d250f730f41015d6e06f0869b
Tom-Kendrick <tom2kendrick@gmail.com>
2023-01-19 22:32:00 +0000

    added new pics

--- theft.py
+++ theft.py
@@ -1,15 +1,30 @@
 import os
 import urllib.request
-import requests
 import time
+import requests
+num = int(input("Input the final number: "))+1
+more = int(input("How many more images do you want: "))
 
-folder = 'real'
+def stealReal(num, more):
+    for i in range(num, num+more):
+        try:
+            url = 'https://www.whichfaceisreal.com/realimages/{:05d}.jpeg'.format(i)
+            save_path = os.path.join("real", '{:05d}.jpeg'.format(i))
+            urllib.request.urlretrieve(url, save_path)
+        except:
+            print("Skipped", i)
 
-if not os.path.exists(folder):
-    os.makedirs(folder)
+def stealFake(num,more):
+    for i in range(num, num+more):
+        try:
+            time.sleep(1)
+            r = requests.get("https://thispersondoesnotexist.com/image", headers={'User-Agent': 'My User Agent 1.0'}).content
+            with open("fake/{:05d}.jpeg".format(i), "wb") as f:
+                f.write(r)
+        except:
+            print("Skipped", i)
 
-for i in range(0, 100):
-    time.sleep(1)
-    r = requests.get("https://thispersondoesnotexist.com/image", headers={'User-Agent': 'My User Agent 1.0'}).content
-    with open("fake/00{:03d}.jpeg".format(i), "wb") as f:
-        f.write(r)
+
+stealReal(num, more)
+stealFake(num, more)
+print("Done", num+more-1)
