737647d949b0d1aa1ceaa357c6e2061a36d5684d
Tom-Kendrick <88669362+Tom-Kendrick@users.noreply.github.com>
2023-03-10 18:44:42 +0000

    Update scr.js

--- static/js/scr.js
+++ static/js/scr.js
@@ -111,12 +111,13 @@
         }
     }
 });
-const blob = document.getElementById("blob");
+const blob = document.getElementById("potential-blob");
 
 
 window.onpointermove = event => { 
 const { clientX, clientY } = event;
 if (!/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    blob.id = "blob"
     blob.animate({
         left: `${clientX}px`,
         top: `${clientY}px`
